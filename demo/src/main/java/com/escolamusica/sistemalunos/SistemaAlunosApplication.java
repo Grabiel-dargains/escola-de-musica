5a9a83cdd8b071bfa370bc3b1e582cabb906183e
gdarg <gabrieldargains@gmail.com>
2025-07-24 09:01:45 -0300

    api

--- demo/src/main/java/com/escolamusica/sistemalunos/SistemaAlunosApplication.java
+++ demo/src/main/java/com/escolamusica/sistemalunos/SistemaAlunosApplication.java
@@ -3,13 +3,13 @@ package com.escolamusica.sistemalunos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Esta anotação é a chave de tudo!
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@OpenAPIDefinition(servers = { @Server(url="/", description="Default Server URL")})
 @SpringBootApplication
 public class SistemaAlunosApplication {
 
-    // Este é o método padrão que o Java procura para iniciar um programa.
     public static void main(String[] args) {
-        // Esta linha diz ao Spring para iniciar a aplicação.
         SpringApplication.run(SistemaAlunosApplication.class, args);
     }
 
